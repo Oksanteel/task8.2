0afd74721d20af2277816af0e3ba4369d5dd20fb
Oksanteel <habanera@mail.ru>
2020-07-16 14:19:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Radio {
+    private int currentRadioStation = 5;
+    private int minRadioStation = 0;
+    private int maxRadioStation = 9;
+    private int currentSoundVolume = 5;
+    private int minSoundVolume = 0;
+    private int maxSoundVolume = 10;
+
+}
+
+
+
